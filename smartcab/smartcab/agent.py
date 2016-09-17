7e997b9af8d4b2bc223852a0537bcc5324b88368
Zhenghe Jin <zjin3@ncsu.edu>
2016-09-17 12:33:04 -0400

    temp

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -11,10 +11,14 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.reward = 0
+        self.next_waypoint = None
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+        self.state = None
+        self.next_waypoint = None
 
     def update(self, t):
         # Gather inputs
@@ -23,16 +27,32 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        
+        is_action = True
+        if self.next_waypoint == 'right' and inputs['light'] == 'red' and inputs['left'] == 'forward':
+            is_action = False
+        elif self.next_waypoint == 'straight' and inputs['light'] == 'red':
+            is_action = False
+        elif self.next_waypoint == 'left':
+            if inputs['light'] == 'red' or inputs['oncoming'] == 'forward' or inputs['oncoming'] == 'right' :
+                is_action = False
+
         # TODO: Select action according to your policy
         action = None
+        if is_action:
+            action = random.choice(Environment.valid_actions)
 
         # Execute action and get reward
+        self.state = inputs
+        self.state['next_waypoint'] = self.next_waypoint
+        self.state = tuple(sorted(self.state.items()))
+
+
         reward = self.env.act(self, action)
+        self.reward = self.reward + reward
 
         # TODO: Learn policy based on state, action, reward
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
 def run():
@@ -41,11 +61,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.01, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
